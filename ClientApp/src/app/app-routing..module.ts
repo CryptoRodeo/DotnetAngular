1ee067dfc398c918b15bf81c93235ee6b489293f
CryptoRodeo <bryan.ramos@hey.com>
2021-07-20 13:55:00 -0400

    Adding files

---
+++ ClientApp/src/app/app-routing..module.ts
@@ -0,0 +1,18 @@
+/**
+ * This top-level module deals with the routing
+ */
+
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full'}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
