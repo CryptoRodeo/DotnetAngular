0292c0af722331e542a742433064dbd25231a8d6
Kilgore Trout <b.r.tech95@gmail.com>
2021-08-13 06:36:13 -0400

    require some modules + libraries, add WIP controller method

--- WorldCities/Controllers/SeedController.cs
+++ WorldCities/Controllers/SeedController.cs
@@ -5,6 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WorldCities.Data;
+using OfficeOpenXml;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using WorldCities.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Security;
+
 namespace WorldCities.Controllers
 {
     [Route("api/[controller]")]
@@ -12,9 +20,16 @@ namespace WorldCities.Controllers
     public class SeedController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        public SeedController(ApplicationDbContext context)
+        private readonly IWebHostEnvironment _env;
+        public SeedController(ApplicationDbContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
+        }
+        [HttpGet]
+        public async Task<ActionResult> Import()
+        {
+            // Todo - > Actually implement this
         }
         
     }
